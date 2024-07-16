8de92d239977b4d99e2ef2e5ddbf55ac6880bcd3
hhs8746 <121233483+hhs8746@users.noreply.github.com>
2024-07-16 21:47:42 +0900

    Time: 72 ms (18.6%), Space: 17 MB (36.78%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,24 +1,41 @@
+from collections import deque
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        #트리에서 DFS 시간 복잡도 O(V+E(노드 + 엣지))
+        #트리에서 BFS 시간 복잡도 O()
         visited = [False]*len(rooms)
         answer=[]
-        def dfs(node):
-            visited[node]=True
-            for nb in rooms[node]:
-                if not visited[nb]:
-                    dfs(nb)
+
+        def bfs(node):
+            queue = deque()
+            queue.append(node)
+            visited[node] = True
+            while queue:
+                cur_v = queue.popleft()
+                for next_v in rooms[cur_v]:
+                    if not visited[next_v]:
+                        visited[next_v]=True
+                        queue.append(next_v)
+
+        bfs(0)
+        return all(visited)
+
+
+        # def dfs(node):
+        #     visited[node]=True
+        #     for nb in rooms[node]:
+        #         if not visited[nb]:
+        #             dfs(nb)
         
-        for i in [0]: #range(len(rooms)):
-            visited = [False]*len(rooms)
-            ans = dfs(i)
-            if False in visited:
-                answer.append(False)
-            else:
-                answer.append(True)
-            print(answer)
+        # for i in [0]: #range(len(rooms)):
+        #     visited = [False]*len(rooms)
+        #     ans = dfs(i)
+        #     if False in visited:
+        #         answer.append(False)
+        #     else:
+        #         answer.append(True)
+        #     print(answer)
             
-        if True not in answer:
-            return False
-        else:
-            return True
+        # if True not in answer:
+        #     return False
+        # else:
+        #     return True
